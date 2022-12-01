102e7029d64c6cf7fdf0fe0a71032645e1188916
Tommy <42751764+tommyothen@users.noreply.github.com>
2022-12-01 19:26:01 +0000

    :sparkles: Reworked day 1 to follow new template
    Made sure to keep the same logic, no matter how much I wanted to change it.

--- years/2022/day1/solution.rs
+++ years/2022/day1/solution.rs
@@ -1,81 +1,98 @@
-//! --- Day 1: Calorie Counting ---
+use std::fs::File;
+use std::io::prelude::*;
 
-fn part1() {
-  // Read the input file
-  let input = std::fs::read_to_string("input.txt").unwrap();
-
-  // Create a score for the most calories seen
-  let mut max_calories = 0;
-
-  // Loop over lines in the input
-  let mut current_elf = 0;
-  for line in input.lines() {
-    // If it's a new line we're finished reading the previous elf
-    if line == "" {
-      if max_calories < current_elf {
-        max_calories = current_elf;
-      }
-
-      // Reset the counter
-      current_elf = 0;
-    } else {
-      let calorie: i32 = line.parse().unwrap();
-
-      current_elf += calorie;
-    }
-  }
-
-  // We check once more at the end in case there isn't a new line at the end of the file
-  if max_calories < current_elf {
-    max_calories = current_elf;
-  }
-
-  // Print the result
-  println!("The top elf is currently carrying {} calories.", max_calories);
+struct Solution {
+    input: String,
 }
 
-fn part2() {
-  let input = std::fs::read_to_string("input.txt").unwrap();
-
-  // Create an array of i32s
-  let mut elves: Vec<i32> = Vec::new();
-
-  // Loop over lines in the input
-  let mut current_elf = 0;
-  for line in input.lines() {
-    // If it's a new line we're finished reading the previous elf
-    if line == "" {
-      elves.push(current_elf);
-
-      // Reset the counter
-      current_elf = 0;
-    } else {
-      let calorie: i32 = line.parse().unwrap();
-
-      current_elf += calorie;
+impl Solution {
+    fn new(input: &str) -> Self {
+        Self {
+            input: input.to_string(),
+        }
     }
-  }
 
-  // If the last line wasn't empty we need to add the last elf
-  if current_elf != 0 {
-    elves.push(current_elf);
-  }
+    fn part1(&self) -> String {
+        // Create a score for the most calories seen
+        let mut max_calories = 0;
 
-  // Sort the elves, biggest to smallest
-  elves.sort_by(|a, b| b.cmp(a));
+        // Loop over lines in the input
+        let mut current_elf = 0;
+        for line in self.input.lines() {
+            // If it's a new line we're finished reading the previous elf
+            if line == "" {
+                if max_calories < current_elf {
+                    max_calories = current_elf;
+                }
 
-  // Print the top 3 elves
-  println!("Top 3 elves:");
-  println!("  1. {}", elves[0]);
-  println!("  2. {}", elves[1]);
-  println!("  3. {}", elves[2]);
-  println!("\nTotal of {} calories.", elves[0] + elves[1] + elves[2]);
+                // Reset the counter
+                current_elf = 0;
+            } else {
+                let calorie: i32 = line.parse().unwrap();
+
+                current_elf += calorie;
+            }
+        }
+
+        // We check once more at the end in case there isn't a new line at the end of the file
+        if max_calories < current_elf {
+            max_calories = current_elf;
+        }
+
+        // Return the result
+        max_calories.to_string()
+    }
+
+    fn part2(&self) -> String {
+        // Create an array of i32s
+        let mut elves: Vec<i32> = Vec::new();
+
+        // Loop over lines in the input
+        let mut current_elf = 0;
+        for line in self.input.lines() {
+            // If it's a new line we're finished reading the previous elf
+            if line == "" {
+                elves.push(current_elf);
+
+                // Reset the counter
+                current_elf = 0;
+            } else {
+                let calorie: i32 = line.parse().unwrap();
+
+                current_elf += calorie;
+            }
+        }
+
+        // If the last line wasn't empty we need to add the last elf
+        if current_elf != 0 {
+            elves.push(current_elf);
+        }
+
+        // Sort the elves
+        elves.sort_by(|a, b| b.cmp(a));
+
+        // Return the result
+        (elves[0] + elves[1] + elves[2]).to_string()
+    }
 }
 
 fn main() {
-  println!("Part 1:");
-  part1();
+    // Read the input
+    let input = std::fs::read_to_string("input.txt").unwrap();
 
-  println!("\nPart 2:");
-  part2();
+    // Create the solution
+    let solution = Solution::new(&input);
+
+    // Run the solutions
+    println!("Part 1: {}", solution.part1());
+    println!("Part 2: {}", solution.part2());
+
+    // Create the output file
+    let mut file = File::create("output.txt").unwrap();
+
+    // Write the solutions to the output file
+    let output = format!("{}\n{}", solution.part1(), solution.part2());
+    file.write_all(output.as_bytes())
+        .map_err(|err| println!("{:?}", err))
+        .ok();
 }
