4c3580477c0ec2cc4bd327a21ea6d5ce4dd8a117
Tommy <42751764+tommyothen@users.noreply.github.com>
2022-12-03 14:36:32 +0000

    :bug: Running the solutions twice

--- years/2022/day2/solution.rs
+++ years/2022/day2/solution.rs
@@ -140,14 +140,15 @@ fn main() {
     let solution = Solution::new(&input);
 
     // Run the solutions
-    println!("Part 1: {}", solution.part1());
-    println!("Part 2: {}", solution.part2());
+    let (part1, part2) = (solution.part1(), solution.part2());
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 
     // Create the output file
     let mut file = File::create("output.txt").unwrap();
 
     // Write the solutions to the output file
-    let output = format!("{}\n{}", solution.part1(), solution.part2());
+    let output = format!("{}\n{}", part1, part2);
     file.write_all(output.as_bytes())
         .map_err(|err| println!("{:?}", err))
         .ok();
