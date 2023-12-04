d6cdf7060e9bad617dbe9285b9a6033570c96db5
Tommy <42751764+tommyothen@users.noreply.github.com>
2023-12-04 22:18:07 +0000

    :star: 2023/04

--- years/2023/04/solution.test.ts
+++ years/2023/04/solution.test.ts
@@ -2,13 +2,23 @@ import { expect, test } from "bun:test";
 import Solution from "./solution";
 
 test("Part 1", async () => {
-  const input = ``;
+  const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 
-  expect(await Solution.part1(input)).toBe(0);
+  expect(await Solution.part1(input)).toBe(13);
 });
 
 test("Part 2", async () => {
-  const input = ``;
+  const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 
-  expect(await Solution.part2(input)).toBe(0);
+  expect(await Solution.part2(input)).toBe(30);
 });
--- years/2023/04/solution.ts
+++ years/2023/04/solution.ts
@@ -2,7 +2,7 @@ import { prettyPrintResults, timeit } from "@/utils";
 import { resolve } from "path";
 
 /**
- * ### Advent of Code 2023 - Day
+ * ### Advent of Code 2023 - Day 04
  */
 export default class Solution {
   private static input: Promise<string> = Bun.file(resolve(import.meta.dir, "./input.txt")).text();
@@ -17,11 +17,66 @@ export default class Solution {
   }
 
   public static async part1(input: string): Promise<number> {
-    return 0;
+    const cards = input.split("\n");
+
+    let result = 0;
+
+    // Loop through each line
+    for (const card of cards) {
+      // Parse out all the lotto numbers
+      const [, lottoNumbers] = card.split(": ");
+      const [winningNumbers, ourNumbers] = lottoNumbers.split(" | ");
+
+      // Create two sets of numbers
+      const numbersRegex = /(\d+)/g;
+      const winningNumbersSet = new Set(winningNumbers.match(numbersRegex));
+      const ourNumbersSet = new Set(ourNumbers.match(numbersRegex));
+
+      // Get the intersection of the two sets
+      const intersection = new Set([...winningNumbersSet].filter(x => ourNumbersSet.has(x)));
+
+      // If there is no intersection, continue
+      if (intersection.size === 0) continue;
+
+      // Calc the "worth" of the card
+      const worth = 2 ** (intersection.size - 1);
+
+      // Add to the result
+      result += worth;
+    }
+
+    return result;
   }
 
   public static async part2(input: string): Promise<number> {
-    return 0;
+    const cards = input.split("\n");
+
+    // Initialize an array to count the number of times each card should be processed
+    let cardCounts = new Array(cards.length).fill(1);
+
+    // Loop through each card
+    for (let i = 0; i < cards.length; i++) {
+      const [, lottoNumbers] = cards[i].split(": ");
+      const [winningNumbers, ourNumbers] = lottoNumbers.split(" | ");
+
+      // Create sets for the numbers
+      const winningNumbersSet = new Set(winningNumbers.match(/\d+/g));
+      const ourNumbersSet = new Set(ourNumbers.match(/\d+/g));
+
+      // Get the intersection of the two sets
+      const intersection = new Set([...winningNumbersSet].filter(x => ourNumbersSet.has(x)));
+
+      // For each matching number, increase the count of subsequent cards
+      let matches = intersection.size;
+      for (let j = i + 1; j <= i + matches && j < cards.length; j++) {
+        cardCounts[j] += cardCounts[i];
+      }
+    }
+
+    // Sum up all the counts
+    const totalScratchcards = cardCounts.reduce((sum, count) => sum + count, 0);
+
+    return totalScratchcards;
   }
 }
 
