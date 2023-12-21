a521d45adccdba0583b7428f14a92b82d0437c8f
GitHub Action <action@github.com>
2023-12-21 00:07:22 +0000

    :calendar: Create challenge for 2023/21

---
+++ years/2023/21/solution.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "bun:test";
+import Solution from "./solution";
+
+test("Part 1", async () => {
+  const input = ``;
+
+  expect(await Solution.part1(input)).toBe(0);
+});
+
+test("Part 2", async () => {
+  const input = ``;
+
+  expect(await Solution.part2(input)).toBe(0);
+});
