9e152ad7b77468e3a184ad8b26e26c0b3e95b66b
GitHub Action <action@github.com>
2024-12-15 00:11:35 +0000

    :calendar: Create challenge for 2024/15

---
+++ years/2024/15/README.md
@@ -0,0 +1,10 @@
+# 🎄 Day 15 -
+
+[Day 15](https://adventofcode.com/2024/day/15)
+
+## Results
+
+| Part | Result | Average of 1000 |
+| ---- | ------ | --------------- |
+| 1    |        |                 |
+| 2    |        |                 |
---
+++ years/2024/15/solution.ts
@@ -0,0 +1,40 @@
+import { prettyPrintResults, timeit } from "@/utils";
+import { resolve } from "path";
+
+/**
+ * ### Advent of Code 2024 - Day 15
+ */
+export default class Solution {
+  private static input: Promise<string> = Bun.file(
+    resolve(import.meta.dir, "./input.txt")
+  ).text();
+
+  public static async solve() {
+    const input = await Solution.input;
+
+    return {
+      part1: await timeit(Solution.part1, [input]),
+      part2: await timeit(Solution.part2, [input]),
+    };
+  }
+
+  public static async part1(input: string): Promise<number> {
+    return 0;
+  }
+
+  public static async part2(input: string): Promise<number> {
+    return 0;
+  }
+}
+
+export async function main(): Promise<[number, number]> {
+  const { part1, part2 } = await Solution.solve();
+  prettyPrintResults([part1, part2]);
+
+  Bun.write(
+    resolve(import.meta.dir, "./output.txt"),
+    `${part1.result}\n${part2.result}`
+  );
+
+  return [part1.result, part2.result];
+}
